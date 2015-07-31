129512219a2afc5cf6ab45595ca560540b71bc1e
MiningMark48 <miningmark48@gmail.com>
2015-07-31 16:16:35 -0400

    Time for an update

--- src/main/java/com/miningmark48/bam/block/BlockBreeder.java
+++ src/main/java/com/miningmark48/bam/block/BlockBreeder.java
@@ -2,12 +2,17 @@ package com.miningmark48.bam.block;
 
 import com.miningmark48.bam.reference.GUIs;
 import com.miningmark48.bam.reference.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class BlockBreeder extends BlockBAM{
 
